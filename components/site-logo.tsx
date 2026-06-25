60df55ea21ddf516b639c81eef0555f6b260e90a
Emanoil Cucu <misteremanoil@gmail.com>
2026-06-25 11:37:38 +0300

    Use provided gold logo mark

--- components/site-logo.tsx
+++ components/site-logo.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import { cn } from '@/lib/utils'
 
@@ -17,14 +18,24 @@ export function SiteLogo({
         className,
       )}
     >
+      <span className="relative h-9 w-11 shrink-0 sm:h-10 sm:w-12" aria-hidden="true">
+        <Image
+          src="/images/site-logo-mark-gold.png"
+          alt=""
+          fill
+          sizes="48px"
+          className="object-contain"
+          priority
+        />
+      </span>
       <span
         className={cn(
           'flex min-w-0 items-center leading-none',
           textClassName,
         )}
       >
-        <span className="whitespace-nowrap font-sans text-[0.98rem] font-semibold tracking-[0.08em] sm:text-[1.05rem]">
-          ALBERT-BENIAMIN CUCU
+        <span className="whitespace-nowrap font-serif text-[1.02rem] font-medium tracking-[0.02em] sm:text-[1.14rem]">
+          Albert-Beniamin Cucu
         </span>
       </span>
     </Link>
