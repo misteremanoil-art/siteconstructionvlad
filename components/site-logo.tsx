fc630c6d27054347c0f986c2a1fca6b2f0d7e875
Emanoil Cucu <misteremanoil@gmail.com>
2026-06-26 14:26:58 +0300

    Rebalance site logo lockup

--- components/site-logo.tsx
+++ components/site-logo.tsx
@@ -16,15 +16,23 @@ export function SiteLogo({
         className,
       )}
     >
-      <span className="relative h-16 w-20 shrink-0 sm:h-[72px] sm:w-[88px]" aria-hidden="true">
+      <span className="relative h-11 w-12 shrink-0 sm:h-12 sm:w-14" aria-hidden="true">
         <Image
-          src="/images/site-logo-gold.png"
+          src="/images/site-logo-mark-gold.png"
           alt=""
           fill
-          sizes="88px"
+          sizes="56px"
           className="object-contain"
           priority
         />
+      </span>
+      <span className="ml-2.5 flex flex-col leading-none">
+        <span className="font-serif text-lg font-semibold text-current transition-colors group-hover:text-brand sm:text-xl">
+          Albert-Beniamin
+        </span>
+        <span className="mt-1 font-serif text-lg font-semibold text-current transition-colors group-hover:text-brand sm:text-xl">
+          Cucu
+        </span>
       </span>
     </Link>
   )
